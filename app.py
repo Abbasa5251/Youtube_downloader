32254f64585429a223837ba520286f6949b7bd7c
Abbas Anandwala <abbasa5251@hotmail.com>
2021-08-25 15:56:14 +0530

    added playlist support

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 from pytube import YouTube
+from pytube import Playlist
 
 def download():
     print("\n\n--------YouTube Video Downloader--------\n\n")
 
     # Link for Youtube Video
     url = input("Enter Your Youtube URL: ")
-    yt = YouTube(url)
-    print(f"Title: {yt.title}")
-    videos = yt.streams.filter(only_audio=True)
-    # Get all Available Youtube Video and Audio Links
-    for i, stream in enumerate(videos):
-        print(f"{str(i)} {str(stream)}")
 
-    num = int(input("\nEnter Your Choice: "))
-    video = videos[num]
-    print("Downloading...")
-    # Download the Specific Link in Specific Path
-    video.download()
-    print("\nDownload Complete.")
+    choice = input("Do you want to download playlist? (y/n): ")
+    if choice.lower() == "y":
+        yt = Playlist(url)
+        print(f'Playlist Name: {yt.title}')
+        print("Downloading...")
+        for i, stream in enumerate(yt.videos):
+            # print(f"{str(i)} {str(stream.title)}")
+            print(stream.streams.get_by_itag(22).title)
+            stream.streams.get_by_itag(22).download()
+        print("\nDownload complete.")
+    else:
+        yt = YouTube(url)
+        print(f"Title: {yt.title}")
+        videos = yt.streams.filter(only_audio=True)
+
+        for i, stream in enumerate(videos):
+            print(f"{str(i)} {str(stream)}")
+
+        num = int(input("\nEnter Your Choice: "))
+        video = videos[num]
+        print("Downloading...")
+        # Download the Specific Link in Specific Path
+        video.download()
+        print("\nDownload Complete.")
 
 if __name__=='__main__':
     # Function Call
